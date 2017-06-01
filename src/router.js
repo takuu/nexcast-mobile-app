f9cd41ec0a8ebe6cf15353425ac24bea6f2c8669
takuu <taku30@gmail.com>
2017-06-01 10:20:47 -0700

    wip

--- src/router.js
+++ src/router.js
@@ -8,12 +8,15 @@ import {
 
 import { NativeRouter, Route, Link } from 'react-router-native'
 import { Provider } from 'react-redux'
+import { Ionicons } from '@expo/vector-icons';
 
 import configureStore from './lib/configureStore'
 
 import Search from './containers/Search';
 import Discover from './containers/Discover';
 import Profile from './containers/Profile';
+import Queue from './containers/Queue';
+import UserPodcasts from './containers/UserPodcasts';
 
 
 function getInitialState () {
@@ -82,39 +85,70 @@ const App = () => (
   <Provider store={store}>
     <NativeRouter>
       <View style={styles.container}>
+        <View style={styles.header}>
+
+        </View>
+        <View style={styles.subContainer}>
+          <Route exact path="/" component={Home}/>
+          <Route path="/about" component={About}/>
+          <Route path="/topics" component={Topics}/>
+
+          <Route path="/queue" component={Queue}/>
+          <Route path="/userpodcasts" component={UserPodcasts}/>
+          <Route path="/search" component={Search}/>
+          <Route path="/discover" component={Discover}/>
+          <Route path="/profile" component={Profile}/>
+        </View>
+
         <View style={styles.nav}>
           <Link
-            to="/"
+            to="/queue"
             underlayColor='#f0f4f7'
             style={styles.navItem}>
-            <Text>Home</Text>
+            <View style={{flex: 1, flexDirection: 'column', alignItems: 'center', alignSelf: 'center'}}>
+              <Ionicons name="logo-buffer" size={32} color="#eee" />
+              <Text style={{color: "#888888", fontSize: 10}}>Queue</Text>
+            </View>
+
+          </Link>
+          <Link
+            to="/userpodcasts"
+            underlayColor='#f0f4f7'
+            style={styles.navItem} >
+            <View style={{flex: 1, flexDirection: 'column', alignItems: 'center', alignSelf: 'center'}}>
+              <Ionicons name="ios-mic" size={32} color="#eee" />
+              <Text style={{color: "#888888", fontSize: 10}}>Podcasts</Text>
+            </View>
           </Link>
           <Link
             to="/search"
             underlayColor='#f0f4f7'
             style={styles.navItem}>
-            <Text>Search</Text>
+            <View style={{flex: 1, flexDirection: 'column', alignItems: 'center', alignSelf: 'center'}}>
+              <Ionicons name="ios-search" size={32} color="#eee" />
+              <Text style={{color: "#888888", fontSize: 10}}>Search</Text>
+            </View>
           </Link>
           <Link
             to="/discover"
             underlayColor='#f0f4f7'
             style={styles.navItem} >
-            <Text>Discover</Text>
+            <View style={{flex: 1, flexDirection: 'column', alignItems: 'center', alignSelf: 'center'}}>
+              <Ionicons name="ios-star" size={32} color="#eee" />
+              <Text style={{color: "#888888", fontSize: 10}}>Discover</Text>
+            </View>
           </Link>
           <Link
             to="/profile"
             underlayColor='#f0f4f7'
             style={styles.navItem} >
-            <Text>Profile</Text>
+            <View style={{flex: 1, flexDirection: 'column', alignItems: 'center', alignSelf: 'center'}}>
+              <Ionicons name="ios-person" size={32} color="#eee" />
+              <Text style={{color: "#888888", fontSize: 10}}>Profile</Text>
+            </View>
           </Link>
-        </View>
 
-        <Route exact path="/" component={Home}/>
-        <Route path="/about" component={About}/>
-        <Route path="/topics" component={Topics}/>
-        <Route path="/search" component={Search}/>
-        <Route path="/discover" component={Discover}/>
-        <Route path="/profile" component={Profile}/>
+        </View>
       </View>
     </NativeRouter>
   </Provider>
@@ -122,15 +156,22 @@ const App = () => (
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 25,
-    padding: 10,
+    padding: 0,
+  },
+  subContainer: {
+    height: '87%'
   },
   header: {
     fontSize: 20,
+    height: '5%',
+    backgroundColor: '#387ef5',
   },
   nav: {
     flexDirection: 'row',
-    justifyContent: 'space-around'
+    justifyContent: 'space-around',
+    alignSelf: 'flex-end',
+    height: 51,
+    backgroundColor: '#ffffff',
   },
   navItem: {
     flex: 1,
